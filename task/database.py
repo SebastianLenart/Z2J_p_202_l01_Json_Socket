60bc719d6de1bcc7105556ab927c868896d109cd
Sebastian <36291550+SebastianLenart@users.noreply.github.com>
2023-10-01 00:09:42 +0200

    half

--- task/database.py
+++ task/database.py
@@ -13,8 +13,48 @@ CREATE_MESSAGE = """CREATE TABLE IF NOT EXISTS message
 id_conversation INTEGER, FOREIGN KEY(id_conversation) REFERENCES conversation (id_conversation));"""
 
 SELECT_LIST_USERS = """select nick from account;"""
+SELECT_BASE_INFO = """select a.nick, a."password", a."admin"  from account a 
+where nick = %s;"""
+SELECT_NICKS_CONVERSATION = """SELECT
+    CASE
+        WHEN %s = c.id_receiver THEN (SELECT aa.nick FROM account aa WHERE aa.id_account = c.id_sender)
+        WHEN %s = c.id_sender THEN (SELECT aa.nick FROM account aa WHERE aa.id_account = c.id_receiver)
+    END AS return_value
+FROM conversation c
+WHERE %s IN (c.id_receiver, c.id_sender);"""
+SELECT_ID_ACCOUNT_BY_NICK = """select id_account from account where nick = %s;"""
+SELECT_COUNT_UNREAD_MESSAGES = """select a.nick, (select aa.nick as _from from
+account aa
+where aa.id_account = (select case 
+	when cc.id_receiver = a.id_account then cc.id_sender
+	else cc.id_receiver
+end as return_value
+from conversation cc 
+where cc.id_conversation = c.id_conversation)), count(m.is_read is null) as unread_messages
+from account a, conversation c 
+inner join message m on m.id_conversation = c.id_conversation 
+where (a.nick = %s and ((c.id_receiver = a.id_account and m.receiver_sender = 'send_to_receiver')
+or (c.id_sender = a.id_account and m.receiver_sender = 'from_receiver')) and m.is_read = False)
+group by a.nick, _from;"""
+SELECT_UNREAD_MESSAGES = """select a.nick, c.id_conversation, m.id_message, (select aa.nick as _from from
+account aa
+where aa.id_account = (select case 
+	when cc.id_receiver = a.id_account then cc.id_sender
+	else cc.id_receiver
+end as return_value
+from conversation cc 
+where cc.id_conversation = c.id_conversation)),
+m."content" as unread_message
+from account a, conversation c 
+inner join message m on m.id_conversation = c.id_conversation 
+where (a.nick = %s and ((c.id_receiver = a.id_account and m.receiver_sender = 'send_to_receiver')
+or (c.id_sender = a.id_account and m.receiver_sender = 'from_receiver')) and m.is_read = FALSE)
+order by c.id_conversation, m."time" ;"""
 INSERT_NEW_USER = """INSERT INTO account (nick, password, admin) 
 VALUES (%s, %s, %s) RETURNING id_account;"""
+UPDATE_UNREAD_MESSAGES = """update message 
+set is_read = True
+where id_message = %s;"""
 
 
 def change_sth_test(connetion):
@@ -30,7 +70,42 @@ def get_list_nicks(connection):
         return cursor.fetchall()
 
 
+def get_base_info(connection, nick):
+    with connection.get_cursor() as cursor:
+        cursor.execute(SELECT_BASE_INFO, (nick,))
+        return cursor.fetchone()
+
+
 def add_new_user(connection, nick, password, admin):
     with connection.get_cursor() as cursor:
         cursor.execute(INSERT_NEW_USER, (nick, password, admin))
         return cursor.fetchall()[0]
+
+
+def get_nicks_conversation(connection, nick="Seba"):
+    with connection.get_cursor() as cursor:
+        cursor.execute(SELECT_ID_ACCOUNT_BY_NICK, (nick,))
+        try:
+            idd = cursor.fetchone()[0]
+            cursor.execute(SELECT_NICKS_CONVERSATION, (idd, idd, idd))
+        except TypeError:
+            return []
+        return cursor.fetchall()
+
+
+def get_counter_unread_messages(connection, nick="Seba"):
+    with connection.get_cursor() as cursor:
+        cursor.execute(SELECT_COUNT_UNREAD_MESSAGES, (nick,))
+        return cursor.fetchall()
+
+
+def get_unread_messages(connection, nick="Seba"):
+    with connection.get_cursor() as cursor:
+        cursor.execute(SELECT_UNREAD_MESSAGES, (nick,))
+        return cursor.fetchall()
+
+
+def update_unread_message(connection, id_message):
+    with connection.get_cursor() as cursor:
+        cursor.execute(UPDATE_UNREAD_MESSAGES, (id_message,))
+
--- task/user_sql.py
+++ task/user_sql.py
@@ -39,43 +39,40 @@ class User:
     def show_base_info_about(self, nick):
         if self.admin != "admin":
             return f"Only admin can get info!"
-        for user in self.users_file["users"]:
-            if user["nick"] == nick:
-                return f"{user['nick']}, {user['password']}, {user['admin']}"
+        with GetConnection() as connection:
+            return database.get_base_info(connection, nick)
 
-    def login(self, nick: str = "default", password: str = "1234"):
-        with open("data.json", "r") as file:
-            self.users_file = json.load(file)
-            if not self.check_user_exists(self.users_file["users"], nick):
+    def login(self, nick: str = "Default", password: str = "1234"):
+        with GetConnection() as connection:
+            if nick not in [element[0] for element in database.get_list_nicks(connection)]:
                 return f"Not found user {nick}"
-            for userr in self.users_file["users"]:
-                if userr["nick"] == nick and userr["password"] == password:
-                    self.set_data_from_json(**userr)
-                    return f"Login"
-                else:
-                    continue
-            return f"Password is wrong"
+            user = database.get_base_info(connection, nick)  # powyzsze linijki sa zbedne przy tym zapisie..
+            if user[0] == nick and user[1] == password:
+                self.set_data_from_db(*user)
+                return f"Login"
+        return f"Password is wrong"
 
-    def check_user_exists(self, list_users, nick):
-        for user_dict in list_users:
-            if user_dict["nick"] == nick:
-                return True
-        # raise SomethingWrong(f"Not found user {nick}")
-        return False
+    # def check_user_exists(self, list_users, nick):
+    #     for user_dict in list_users:
+    #         if user_dict["nick"] == nick:
+    #             return True
+    #     # raise SomethingWrong(f"Not found user {nick}")
+    #     return False
 
-    def set_data_from_json(self, nick="default", password="default", admin="No admin",
-                           messages="default"):
+    def set_data_from_db(self, nick="default", password="default", admin="False"):
         self.nick = nick
         self.password = password
         self.admin = admin
-        self.messages = messages
 
-    def print_nick(self):
-        print(self.nick, self.password, self.admin)
+    # def print_nick(self):
+    #     print(self.nick, self.password, self.admin)
 
     def show_conversation(self, nick):
-        self.check_do_u_have_this_nick_in_conversation(nick, self.messages)
-        self.check_unread_messages()  # odczytuje nieodczytane, ale wszystkie, nie tylko od tego konkretnego usera...
+        if not self.check_do_u_have_this_nick_in_conversation(nick):
+            print("You dont have this nick in conversation")
+            return
+        self.check_unread_messages()
+        self.read_unread_messages()
         sorted_messages = self.sort_messages_by_date(nick)
         print(f"Conversation with {nick}")
         return_list = []
@@ -94,24 +91,22 @@ class User:
                 return text["text"]
 
     def check_unread_messages(self):
-        return_unread_texts = []
-        for text in self.messages:
-            if len(text['unread']) == 0:
-                continue
-            print(f"You have {len(text['unread'])} unread messages from {text['with']}: ")
-            return_unread_texts = self.read_unread_messages(text)  # we deliver 1 dictionary
-            self.sort_messages_by_date(text['with'])
-            text["unread"].clear()
-        self.update_messages_in_json_file()
-        return return_unread_texts
+        with GetConnection() as connection:
+            counter_unread = database.get_counter_unread_messages(
+                connection)  # dodaj self.nick ale dopiero po zalogowaniu!!!
+            if len(counter_unread) == 0:
+                return []
+            for unread in counter_unread:
+                print(f"You {unread[0]} have {unread[2]} messages from {unread[1]}")
+            return counter_unread
 
-    def read_unread_messages(self, messages):
-        return_list = []
-        for num, text in enumerate(messages["unread"]):
-            print(num + 1, text)
-            return_list.append(text)
-            self.add_to_read_text(text, messages)
-        return return_list
+    def read_unread_messages(self):
+        with GetConnection() as connection:
+            unread_messages = database.get_unread_messages(connection)  # dodaj self.nick ale dopiero po zalogowaniu!!!
+            for text in unread_messages:
+                print(f"{text[0]} have unread message(s) from {text[3]}: {text[4]}")
+                database.update_unread_message(connection, text[2])
+        return unread_messages
 
     def add_to_read_text(self, text, messages):
         messages["text"].append(text)
@@ -148,16 +143,19 @@ class User:
         self.update_messages_in_json_file()
         return f"Send ok"
 
-    def check_do_u_have_this_nick_in_conversation(self, nick, messages):
-        for mess_disct in messages:
-            if mess_disct["with"] == nick:
-                return mess_disct
-        self.messages.append({
-            "with": nick,
-            "unread": [],
-            "text": []
-        })
-        return self.messages[-1]
+    def check_do_u_have_this_nick_in_conversation(self, nick):
+        with GetConnection() as connection:
+            list_users = database.get_nicks_conversation(connection)  # dodaj self.nick ale dopiero po zalogowaniu!!!
+            for user in list_users:
+                if nick == user[0]:
+                    return True
+            return False
+            # self.messages.append({
+            #     "with": nick,
+            #     "unread": [],
+            #     "text": []
+            # })
+            # return self.messages[-1]
 
     def save_unread_text_in_receiver(self, nick, text: list):
         for user_dict in self.users_file["users"]:
@@ -168,8 +166,7 @@ class User:
 
 if __name__ == '__main__':
     user = User()
-    pprint(user.show_list_users())
-
+    user.read_unread_messages()
 """
 if __name__ == '__main__':  # !!! bez tego ponizsze linijki beda wywolywane gdy gdzies uzyjemy 'from user import User'
     user = User()
